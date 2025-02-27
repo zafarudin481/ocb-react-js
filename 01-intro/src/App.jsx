df668defc35d2449ed994e6015a3cf00d261531c
zafarudin481 <zafarudin.jaffir@gmail.com>
2025-02-27 21:33:28 +0800

    init

---
+++ 01-intro/src/App.jsx
@@ -0,0 +1,17 @@
+
+function App() {
+  const gaya = {
+    color: 'yellow',
+    fontSize: '3rem'
+  };
+
+  return (
+    <div>
+      <p style={gaya}>Nama</p>
+      <p>Saya</p>
+      <p>Zafarudin</p>
+    </div>
+  )
+}
+
+export default App
