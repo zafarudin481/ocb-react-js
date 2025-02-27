1afafd19a6ece85faf3e4159e4224ac7668a34a8
zafarudin481 <zafarudin.jaffir@gmail.com>
2025-02-28 00:23:47 +0800

    my first react project

--- 01-intro/src/App.jsx
+++ 01-intro/src/App.jsx
@@ -1,15 +1,20 @@
 
 function App() {
-  const gaya = {
-    color: 'yellow',
-    fontSize: '3rem'
+  const stailBlue = {
+    color: 'blue',
+    fontSize: '2.5rem',
+    textAlign: 'center'
+  };
+  const stailGreen = {
+    color: 'green',
+    fontSize: '2.5rem',
+    textAlign: 'center'
   };
 
   return (
     <div>
-      <p style={gaya}>Nama</p>
-      <p>Saya</p>
-      <p>Zafarudin</p>
+      <p style={stailBlue}>My name is Zafarudin.</p>
+      <p style={stailGreen}>This is my first React Project.</p>
     </div>
   )
 }
