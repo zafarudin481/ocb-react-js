e43466909d3dd0e64a6e4222452e2d6ae0f70060
zafarudin481 <zafarudin.jaffir@gmail.com>
2025-03-08 09:49:33 +0800

    using CSS module to avoid unintended CSS pollution across components

--- 04-ocb24/src/components/Counter.jsx
+++ 04-ocb24/src/components/Counter.jsx
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import { useState } from 'react';
+import styles from './Counter.module.css';
 
 const Counter = props => {
     const [count, setCount] = useState(0);
@@ -15,8 +16,8 @@ const Counter = props => {
             alignItems: 'center',
             gap: '3px'
         }}>
-            <p>Hello {props.name}</p>
-            <p>Counter: {count}</p>
+            <p className={styles.title}>Hello {props.name}</p>
+            <p className={styles.description}>Counter: {count}</p>
             <button onClick={handleClick}>Add 1</button>
         </div >
     )
